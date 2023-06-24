fac55693323e7c200924ad3fea976e723fe6fcda
ukyon <kyoyannnnn@gmail.com>
2023-06-25 00:19:08 +0900

    fix

--- main.go
+++ main.go
@@ -67,6 +67,7 @@ func realMain() error {
 	go func() {
 		defer wg.Done()
 		<-ctx.Done()
+		//Shutdownで無期限に処理終了を待機しないように有効期限のあるcontextを渡す
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		if err := s.Shutdown(ctx); err != nil {
